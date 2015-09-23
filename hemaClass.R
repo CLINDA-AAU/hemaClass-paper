69034711ecdd06ee39e48abe88fff33c9789ad23
Falgreen <falgreen@me.com>
2015-09-23 21:51:42 +0200

    Added temp comparison

--- hemaClass.R
+++ hemaClass.R
@@ -84,6 +84,40 @@ normalizer <- function(study, gse, nsamples = 30, global = FALSE) {
   rma <<- rma
 }
 
+#function for comparing results
+testfun <- function(x, y, dec = 2, weight){
+  a <- table(x, y)
+  acc <- binom.test(c(sum(diag(a)),sum(a)-sum(diag(a))))
+  acc <- paste(round(acc$estimate, dec),
+               " (", paste(round(acc$conf.int, dec), collapse = ","), ")", sep = "")
+
+  kap <- cohen.kappa(data.frame(x,y), w=weight)
+  conf.1 <- kap$confid[2,1]
+  conf.2 <- min(kap$confid[2,3],1)
+  kappa <- paste(round(kap$weighted.kappa, dec),
+                 " (", round(conf.1, dec), ",", round(conf.2, dec), ")", sep = "")
+
+  c(acc, kappa)
+}
+
+weightFun <- function(n){
+  weight <- matrix(1, n, n)
+  weight[ncol(weight), ] <- 0.5
+  weight[, nrow(weight)] <- 0.5
+  diag(weight) <- 0
+  return(weight)
+}
+
+testfunCor <- function(x, y){
+
+  logit <- function(p) log(p/(1-p))
+
+  cort <- cor.test(logit(x), logit(y))
+
+  leg <- paste0(round(cort$estimate, 2),
+                " (", paste(round(cort$conf.int, 2), collapse = ","), ")")
+  return(leg)
+}
 
 ################################################################################
 # Download and prepare the data
@@ -114,11 +148,15 @@ for (gse in as.character(studies$GSE)) {
 if (!exists("rma", inherits = FALSE) || recompute) {
   rma <- list()
 }
+
+
+
 rma[["cohort"]][["LLMPPCHOP"]]  <- microarrayScale(exprs(dat$GSE10846$es$CHOP))
 rma[["cohort"]][["LLMPPRCHOP"]] <- microarrayScale(exprs(dat$GSE10846$es$'R-CHOP'))
 rma[["cohort"]][["MDFCI"]]      <- microarrayScale(exprs(dat$GSE34171$es$GPL570))
 rma[["cohort"]][["IDRC"]]       <- microarrayScale(exprs(dat$GSE31312$es$Batch1))
 rma[["cohort"]][["CHEPRETRO"]]  <- microarrayScale(exprs(dat$GSE56315$es$DLBCL))
+
 
 ################################################################################
 # RMA normalization
@@ -144,35 +182,78 @@ if (is.null(rma$reference) || recompute) {
 # Establish the results
 ################################################################################
 
+studies.vec <- c("CHEPRETRO", "MDFCI", "IDRC", "LLMPPRCHOP", "LLMPPCHOP")
+names(studies.vec) <- c("CHEPRETRO", "MDFCI", "IDRC", "LLMPP R-CHOP", "LLMPP CHOP")
+
 results <- list()
-for (study in c("LLMPPCHOP", "LLMPPRCHOP", "MDFCI", "CHEPRETRO")) {
+for (study in studies.vec) {
 
   if (study != "LLMPPCHOP") {
 
-    results[["ABCGCB"]][[study]]$cohort <- ABCGCB(rma$cohort[[study]])
-    results[["BAGS"]][[study]]$cohort   <- BAGS(rma$cohort[[study]])
-    results[["REGS"]][[study]]$cohort   <- ResistanceClassifier(rma$cohort[[study]])
+    results[["ABCGCB"]][[study]]$onebyone <- ABCGCB(rma$onebyone[[study]])
+    results[["BAGS"]][[study]]$onebyone   <- BAGS(rma$onebyone[[study]])
+    results[["REGS"]][[study]]$onebyone   <- ResistanceClassifier(rma$onebyone[[study]])
 
   }
 
   # ABC/GCB
   results[["ABCGCB"]][[study]]$refbased <- ABCGCB(rma$refbased[[study]])
-  results[["ABCGCB"]][[study]]$onebyone <- ABCGCB(rma$onebyone[[study]])
+  results[["ABCGCB"]][[study]]$cohort   <- ABCGCB(rma$cohort[[study]])
 
   # BAGS
   results[["BAGS"]][[study]]$refbased <- BAGS(rma$refbased[[study]])
-  results[["BAGS"]][[study]]$onebyone <- BAGS(rma$onebyone[[study]])
+  results[["BAGS"]][[study]]$cohort   <- BAGS(rma$cohort[[study]])
 
   # REGS
   # The classifier for Cyclophosphamide, Doxorubicin, and Vincristine:
   results[["REGS"]][[study]]$refbased <- ResistanceClassifier(rma$refbased[[study]])
-  results[["REGS"]][[study]]$onebyone <- ResistanceClassifier(rma$onebyone[[study]])
+  results[["REGS"]][[study]]$cohort   <- ResistanceClassifier(rma$cohort[[study]])
 
 }
+
+
 
 ################################################################################
 # Comparisons of the results
 ################################################################################
+comparison <- list()
+
+comp.matrix <- matrix("-", nrow = length(studies.vec), ncol = 3,
+                      dimnames = list(studies.vec,
+                                      c("Accuracy", "Kappa", "rho")))
+
+# Add the weights for Cohens Kappa
+comparison[["ABCGCB"]]$weight <- matrix(c(0,1,2,1,0,1,2,1,0), 3) / 2
+comparison[["BAGS"]]$weights  <- weightFun(6)
+comparison[["REGS"]]$weights  <- weightFun(3)
+
+comparison[["ABCGCB"]][["onebyone"]]$result <- comp.matrix
+comparison[["ABCGCB"]][["refbased"]]$result <- comp.matrix
+
+comparison[["BAGS"]][["onebyone"]]$result <- comp.matrix
+comparison[["BAGS"]][["refbased"]]$result <- comp.matrix
+
+comparison[["REGS"]][["onebyone"]]$result <- comp.matrix
+comparison[["REGS"]][["refbased"]]$result <- comp.matrix
+
+
+for(study in studies.vec){
+  comparison[["ABCGCB"]][["onebyone"]][[study]]<- table(c("ABC", "NC", "GCB"), c("ABC", "NC", "GCB"))
+  comparison[["ABCGCB"]][["refbased"]][[study]] <- table(c("ABC", "NC", "GCB"), c("ABC", "NC", "GCB"))
+
+  comparison[["BAGS"]][["onebyone"]][[study]] <- table(c("N", "CB", "CC", "M", "PB"), c("N", "CB", "CC", "M", "PB"))
+  comparison[["BAGS"]][["refbased"]][[study]] <- table(c("N", "CB", "CC", "M", "PB"), c("N", "CB", "CC", "M", "PB"))
+
+  for(drug in c("Cyclophosphamide", "Doxorubicin", "Vincristine", "Combined")){
+    comparison[["REGS"]][["onebyone"]][[study]][[drug]] <- table(c("Sensitive", "Intermediate", "Resistant"), c("Sensitive", "Intermediate", "Resistant"))
+    comparison[["REGS"]][["refbased"]][[study]][[drug]] <- table(c("Sensitive", "Intermediate", "Resistant"), c("Sensitive", "Intermediate", "Resistant"))
+  }
+}
+
+
+
+
+
 
 
 
